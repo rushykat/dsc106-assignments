2f985cb3f1771e0f55142e2223bdde2f79fd63e1
Rushi Katabathuni <115051948+rushykat@users.noreply.github.com>
2025-04-29 21:47:41 -0700

    added project 2 report to projects page

--- projects/projects.js
+++ projects/projects.js
@@ -10,3 +10,22 @@ const projectTitle = document.querySelector(".projects-title");
 projectTitle.innerHTML = `
     ${projects.length} Projects
 `;
+
+// Adding link for project 2 report clickable from 
+const reportWrapper = document.createElement("a");
+reportWrapper.href = `./police/project2_report.html`;
+const p = projects[5];
+reportWrapper.innerHTML = `
+    <article>
+        <h3>${p.title}</h3>
+        <img src="${p.image}">
+        <p>${p.description}</p>
+    </article>
+
+`
+document.querySelector(".projects").appendChild(reportWrapper);
+
+// console.log(projects[5]);
+// for (let i = 0; i < projects.length; i++) {
+//     console.log(projects[i]);
+// }
