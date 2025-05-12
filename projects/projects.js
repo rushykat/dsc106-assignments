713c9f003c85c494cfa900a9f75907a16a4e9c83
Rushi Katabathuni <115051948+rushykat@users.noreply.github.com>
2025-05-12 15:36:43 -0700

    Revert "testing project hosting"
    This reverts commit 9e3d737407e94e22938c59cfee6bd8844a58642a.

--- projects/projects.js
+++ projects/projects.js
@@ -1,8 +1,8 @@
-import { fetchJSON, renderProjects } from "/global.js";
+import { fetchJSON, renderProjects } from "../global.js";
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 // fetch projects and render
-const projects = await fetchJSON('/lib/projects.json');
+const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector(".projects");
 renderProjects(projects, projectsContainer, 'h2');
 
